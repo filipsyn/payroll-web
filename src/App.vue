af7dfe5633d5fe0f5b4bcbb539d7e6ebe6291042
Filip S <filip.syn@protonmail.com>
2023-07-07 17:58:31 +0200

    Add basic app bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <v-app>
+    <v-app-bar
+      color="teal-darken-2"
+    >
+      <v-app-bar-nav-icon>
+      </v-app-bar-nav-icon>
+
+      <v-app-bar-title>
+        Payroll
+      </v-app-bar-title>
+    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>

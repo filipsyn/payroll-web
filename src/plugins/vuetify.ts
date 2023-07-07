3bf2dc43ef1788279b82d3083daa30e0c6446ff2
Filip S <filip.syn@protonmail.com>
2023-07-07 18:45:46 +0200

    Set Vuetify blueprint to Material Design 3

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,11 +8,11 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
-// Composables
-import { createVuetify } from 'vuetify'
+import {createVuetify} from 'vuetify'
+import {md3} from "vuetify/blueprints/md3";
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  blueprint: md3,
   theme: {
     themes: {
       light: {
